22d9c1dac3bda712871b8fd52eb20367b8fb6e9f
Raj Patel <rkpatel4@ncsu.edu>
2021-03-27 23:13:41 -0400

    #1, Implemented working simple command line ui

--- SimpleCalculator/SimpleCalculator.py
+++ SimpleCalculator/SimpleCalculator.py
@@ -15,8 +15,50 @@ def division(a, b):
     return x
 
 def main():
-    x = addition(2, 3)
-    print(x)
+    print("Welcome to Simple Calculator!")
+    print("This calculator allows you to add, subtract, multiply, and divide.")
+    
+    firstNum = False
+    while firstNum == False:
+        num1 = input("Enter first number: ")
+        try:
+            num1 = float(num1)
+        except:
+            print("Not a number.")
+            continue
+        firstNum = True 
+
+    valid = False
+    while valid == False:
+        print("Operators include: Addition \"+\"; Subtraction \"-\"; Multiplication \"*\"; Division \"/\"; ")
+        operator = input("Enter operator: ")
+        if operator == "+" or operator == "-" or operator == "*" or operator == "/":
+            valid = True
+        else:
+            print("Not a valid operator.")
+            continue
+
+    secondNum = False
+    while secondNum == False:
+        num2 = input("Enter second number: ")
+        try:
+            num2 = float(num2)
+        except:
+            print("Not a number.")
+            continue
+        secondNum = True 
+
+    answer = 0
+    if operator == "+":
+        answer = addition(num1, num2)
+    elif operator == "-":
+        answer = subtraction(num1, num2)
+    elif operator == "*":
+        answer = multiplication(num1, num2)
+    elif operator == "/":
+        answer = division(num1, num2)
+
+    print("Answer = ", answer)
 
 if __name__ == "__main__":
     main()
