9d49b8bfe3171ee23999ee78a0b06870222cd7d9
Raj Patel <rkpatel4@ncsu.edu>
2021-03-27 22:27:48 -0400

    #6 Completed Testing of all 4 methods.

---
+++ SimpleCalculator/SimpleCalculatorTest.py
@@ -0,0 +1,60 @@
+import unittest
+import SimpleCalculator as sc
+
+class SimpleCalculatorTest(unittest.TestCase):
+
+    def test_addition(self): 
+        # Passing Test
+        self.assertEqual(sc.addition(2, 3), 5)
+        self.assertEqual(sc.addition(50, 40), 90)
+        self.assertEqual(sc.addition(3.567, 9.876), 13.443)
+        self.assertEqual(sc.addition(-2, -3), -5)
+
+        #Failing Test
+        self.assertNotEqual(sc.addition(2, 3), 6)
+        self.assertNotEqual(sc.addition(50, 40), 4000)
+        self.assertNotEqual(sc.addition(3.567, 9.876), 15)
+        self.assertNotEqual(sc.addition(-2, -3), -10)
+
+    def test_subtraction(self): 
+        # Passing Test
+        self.assertEqual(sc.subtraction(5, 3), 2)
+        self.assertEqual(sc.subtraction(9678, 40), 9638)
+        self.assertEqual(sc.subtraction(9.876, 3.567), 6.308999999999999)
+        self.assertEqual(sc.subtraction(4, 10), -6)
+
+        #Failing Test
+        self.assertNotEqual(sc.subtraction(2, 3), 6)
+        self.assertNotEqual(sc.subtraction(50, 40), 4000)
+        self.assertNotEqual(sc.subtraction(3.567, 9.876), 15)
+        self.assertNotEqual(sc.subtraction(4, 10), -20)
+
+    def test_multiplication(self): 
+        # Passing Test
+        self.assertEqual(sc.multiplication(2, 3), 6)
+        self.assertEqual(sc.multiplication(50, 40), 2000)
+        self.assertEqual(sc.multiplication(9.876, 3.567), 35.227692)
+        self.assertEqual(sc.multiplication(4, -10), -40)
+
+        #Failing Test
+        self.assertNotEqual(sc.multiplication(2, 3), 5)
+        self.assertNotEqual(sc.multiplication(50, 40), 90)
+        self.assertNotEqual(sc.multiplication(3.567, 9.876), 15)
+        self.assertNotEqual(sc.multiplication(4, -10), 20)
+
+    def test_division(self): 
+        # Passing Test
+        self.assertEqual(sc.division(3, 2), 1.5)
+        self.assertEqual(sc.division(2, 3), 0.6666666666666666)
+        self.assertEqual(sc.division(6000, 20), 300)
+        self.assertEqual(sc.division(9.876, 3.567), 2.7687132043734226)
+        self.assertEqual(sc.division(4, -10), -2/5)
+
+        #Failing Test
+        self.assertNotEqual(sc.division(2, 3), 5)
+        self.assertNotEqual(sc.division(50, 40), 90)
+        self.assertNotEqual(sc.division(3.567, 9.876), 15)
+        self.assertNotEqual(sc.division(4, -10), 20)
+
+if __name__ == '__main__':
+    unittest.main()
