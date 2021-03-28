5e8310ae4aef8e35913697f4e869927bcca2676d
Raj Patel <rkpatel4@ncsu.edu>
2021-03-27 22:00:14 -0400

    #2, #3, #4, #5 Finished Implementation

---
+++ SimpleCalculator/SimpleCalculator.py
@@ -0,0 +1,22 @@
+def addition(a, b):
+    x = a + b
+    return x
+
+def subtraction(a, b):
+    x = a - b
+    return x
+
+def multiplication(a, b):
+    x = a * b
+    return x
+
+def division(a, b):
+    x = a / b
+    return x
+
+def main():
+    x = addition(2, 3)
+    print(x)
+
+if __name__ == "__main__":
+    main()
